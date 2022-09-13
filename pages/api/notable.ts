c6959e3dc118d129aacad29044445fa9caea4600
Adam Jackson <akjackson1@gmail.com>
2022-09-13 07:54:25 -0400

    Catch errors for notable birds

--- pages/api/notable.ts
+++ pages/api/notable.ts
@@ -31,47 +31,51 @@ type Response = {
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { region } = req.query;
+  try {
+    const { region } = req.query;
 
-  const response = await fetch(
-    `https://api.ebird.org/v2/data/obs/${region}/recent/notable?detail=full&back=7&key=${process.env.NEXT_PUBLIC_EBIRD_API}`
-  );
-  let reports: Response[] = await response.json();
+    const response = await fetch(
+      `https://api.ebird.org/v2/data/obs/${region}/recent/notable?detail=full&back=7&key=${process.env.NEXT_PUBLIC_EBIRD_API}`
+    );
+    let reports: Response[] = await response.json();
 
-  if (!reports?.length) {
-    res.status(200).json([]);
-  }
-
-  reports = reports
-    //Remove duplicates. For unknown reasons, eBird sometimes returns duplicates
-    .filter((value, index, array) => array.findIndex((searchItem) => searchItem.obsId === value.obsId) === index)
-    .filter(({ comName }) => !comName.includes("(hybrid)"));
-
-  const reportsBySpecies: any = {};
-
-  reports.forEach((item) => {
-    if (!reportsBySpecies[item.speciesCode]) {
-      reportsBySpecies[item.speciesCode] = {
-        name: item.comName,
-        code: item.speciesCode,
-        reports: [],
-      };
+    if (!reports?.length) {
+      res.status(200).json([]);
     }
-    reportsBySpecies[item.speciesCode].reports.push({
-      id: item.obsId,
-      location: item.locName,
-      date: item.obsDt,
-      checklistId: item.subId,
-      lat: item.lat,
-      lng: item.lng,
-      hasRichMedia: item.hasRichMedia,
-      countyName: item.subnational2Name,
-      userDisplayName: item.userDisplayName,
-      approved: item.obsValid,
+
+    reports = reports
+      //Remove duplicates. For unknown reasons, eBird sometimes returns duplicates
+      .filter((value, index, array) => array.findIndex((searchItem) => searchItem.obsId === value.obsId) === index)
+      .filter(({ comName }) => !comName.includes("(hybrid)"));
+
+    const reportsBySpecies: any = {};
+
+    reports.forEach((item) => {
+      if (!reportsBySpecies[item.speciesCode]) {
+        reportsBySpecies[item.speciesCode] = {
+          name: item.comName,
+          code: item.speciesCode,
+          reports: [],
+        };
+      }
+      reportsBySpecies[item.speciesCode].reports.push({
+        id: item.obsId,
+        location: item.locName,
+        date: item.obsDt,
+        checklistId: item.subId,
+        lat: item.lat,
+        lng: item.lng,
+        hasRichMedia: item.hasRichMedia,
+        countyName: item.subnational2Name,
+        userDisplayName: item.userDisplayName,
+        approved: item.obsValid,
+      });
     });
-  });
 
-  const species = Object.entries(reportsBySpecies).map(([key, value]) => value);
+    const species = Object.entries(reportsBySpecies).map(([key, value]) => value);
 
-  res.status(200).json([...species]);
+    res.status(200).json([...species]);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 }
