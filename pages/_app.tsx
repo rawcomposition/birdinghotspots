99413bc4988e877804851716304e501254082065
Adam Jackson <akjackson1@gmail.com>
2022-09-15 12:35:05 -0400

    Add progress indicator

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,14 @@ import Footer from "components/Footer";
 import Header from "components/Header";
 import { UserProvider } from "providers/user";
 import { Toaster } from "react-hot-toast";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
       <Toaster containerStyle={{ zIndex: 10001 }} />
       <Header />
+      <NextNProgress height={1} />
       <Component {...pageProps} />
       <Footer />
     </UserProvider>
