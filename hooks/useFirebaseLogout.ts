2e3c8f5c9a6434b1a91bf52819db0a59783b05a5
Adam Jackson <akjackson1@gmail.com>
2022-09-23 09:02:47 -0400

    Update useFirebaseLogout.ts

--- hooks/useFirebaseLogout.ts
+++ hooks/useFirebaseLogout.ts
@@ -2,16 +2,21 @@ import * as React from "react";
 import { auth } from "lib/firebaseAuth";
 import { signOut } from "firebase/auth";
 import useSecureFetch from "hooks/useSecureFetch";
+import { useRouter } from "next/router";
+import toast from "react-hot-toast";
 
 export default function useFirebaseLogout() {
   const [loading, setLoading] = React.useState(false);
   const secureFetch = useSecureFetch();
+  const router = useRouter();
 
   const logout = async () => {
     setLoading(true);
     try {
       await secureFetch("/api/auth/logout", "post");
       await signOut(auth);
+      toast.success("Logged out");
+      router.push("/");
     } catch (error) {
       console.error(error);
     } finally {
