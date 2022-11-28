d96b4fba592fbb96528a24d30c07befd71d13f76
Adam Jackson <akjackson1@gmail.com>
2022-11-28 16:15:20 -0600

    Fix featured image bug

--- components/FeaturedImage.tsx
+++ components/FeaturedImage.tsx
@@ -12,10 +12,10 @@ type Props = {
 
 export default function FeaturedImage({ photos }: Props) {
   const [index, setIndex] = React.useState(0);
+  const items = photos.map((photo) => processImg(photo));
+  const indexRef = React.useRef(index);
+  indexRef.current = index;
   if (photos.length === 0) return null;
-  const items = photos
-    .filter(({ isStreetview }, index) => !(index === 0 && isStreetview))
-    .map((photo) => processImg(photo));
 
   const handlePrev = (e: React.MouseEvent) => {
     e.preventDefault();
@@ -35,61 +35,61 @@ export default function FeaturedImage({ photos }: Props) {
     setIndex(index + 1);
   };
 
-  const current = photos[index || 0];
-  const isStreetview = current.isStreetview;
-
   return (
     <Gallery options={{ dataSource: items }} uiElements={uiElements} withCaption>
-      <Item {...processImg(current)}>
-        {({ ref, open }) => {
-          const imgRef = ref as any;
-          return (
-            <div className="relative group">
-              {isStreetview ? (
-                <StreetView
-                  className="w-full h-[250px] sm:h-[350px] md:h-[450px] rounded-lg mb-8 -mt-10"
-                  {...current.streetviewData}
-                  onClick={open}
-                />
-              ) : (
-                <img
-                  ref={imgRef}
-                  src={current.lgUrl || current.smUrl}
-                  width={current.width}
-                  height={current.height}
-                  className="w-full h-[250px] sm:h-[350px] md:h-[450px] object-cover object-center rounded-lg mb-8 -mt-10 cursor-pointer"
-                  onClick={open}
-                />
-              )}
-              {items.length > 1 && (
-                <>
-                  <button
-                    type="button"
-                    className="absolute top-4 right-4 flex items-center gap-2 px-3 py-0.5 text-sm font-medium bg-white  hover:opacity-100 opacity-80 rounded-sm transition-opacity"
+      {items.map((item, i) => (
+        <Item key={photos[i].originalUrl} {...item}>
+          {({ ref, open }) => {
+            const imgRef = ref as any;
+            return (
+              <div className={`relative group ${index === i ? "block" : "hidden"}`} ref={imgRef}>
+                {photos[i].isStreetview ? (
+                  <StreetView
+                    className="w-full h-[250px] sm:h-[350px] md:h-[450px] rounded-lg mb-8 -mt-10"
+                    {...photos[i].streetviewData}
                     onClick={open}
-                  >
-                    {items.length} photos
-                  </button>
-                  <button
-                    type="button"
-                    className="absolute bg-white w-8 h-8 rounded-full pl-0.5 cursor-pointer top-1/2 right-[0.85rem] -translate-y-1/2 flex items-center justify-center opacity-50 group-hover:opacity-60 group-hover:right-4 transition-all hover:!opacity-80"
-                    onClick={handleNext}
-                  >
-                    <ChevronRightIcon className="w-6 h-6" />
-                  </button>
-                  <button
-                    type="button"
-                    className="absolute bg-white w-8 h-8 rounded-full pr-0.5 cursor-pointer top-1/2 left-[0.85rem] -translate-y-1/2 flex items-center justify-center opacity-50 group-hover:opacity-60 group-hover:left-4 transition-all hover:!opacity-80"
-                    onClick={handlePrev}
-                  >
-                    <ChevronLeftIcon className="w-6 h-6" />
-                  </button>
-                </>
-              )}
-            </div>
-          );
-        }}
-      </Item>
+                  />
+                ) : (
+                  <img
+                    ref={imgRef}
+                    //Prevent loading images unless viewed
+                    src={index === i ? photos[i].lgUrl || photos[i].smUrl : ""}
+                    width={item.width}
+                    height={item.height}
+                    className="w-full h-[250px] sm:h-[350px] md:h-[450px] object-cover object-center rounded-lg mb-8 -mt-10 cursor-pointer"
+                    onClick={open}
+                  />
+                )}
+                {items.length > 1 && (
+                  <>
+                    <button
+                      type="button"
+                      className="absolute top-4 right-4 flex items-center gap-2 px-3 py-0.5 text-sm font-medium bg-white  hover:opacity-100 opacity-80 rounded-sm transition-opacity"
+                      onClick={open}
+                    >
+                      {items.length} photos
+                    </button>
+                    <button
+                      type="button"
+                      className="absolute bg-white w-8 h-8 rounded-full pl-0.5 cursor-pointer top-1/2 right-[0.85rem] -translate-y-1/2 flex items-center justify-center opacity-50 group-hover:opacity-60 group-hover:right-4 transition-all hover:!opacity-80"
+                      onClick={handleNext}
+                    >
+                      <ChevronRightIcon className="w-6 h-6" />
+                    </button>
+                    <button
+                      type="button"
+                      className="absolute bg-white w-8 h-8 rounded-full pr-0.5 cursor-pointer top-1/2 left-[0.85rem] -translate-y-1/2 flex items-center justify-center opacity-50 group-hover:opacity-60 group-hover:left-4 transition-all hover:!opacity-80"
+                      onClick={handlePrev}
+                    >
+                      <ChevronLeftIcon className="w-6 h-6" />
+                    </button>
+                  </>
+                )}
+              </div>
+            );
+          }}
+        </Item>
+      ))}
     </Gallery>
   );
 }
