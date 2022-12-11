4d8604d8cc7c740c84f969a2349ae1de57294efe
Adam Jackson <akjackson1@gmail.com>
2022-12-11 12:38:26 -0500

    Improve header UI

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import NavItem from "components/NavItem";
 import Link from "next/link";
 import SearchModal from "components/SearchModal";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
@@ -8,63 +7,46 @@ import { useRouter } from "next/router";
 
 const links = [
   {
-    label: "Explore",
+    label: "Explore Hotspots",
     href: "/explore",
-  },
-  {
-    label: "Contact",
-    href: "/contact",
   },
 ];
 
 export default function Header() {
-  let [collapsed, setCollapsed] = React.useState<boolean>(false);
   const [showSearch, setShowSearch] = React.useState<boolean>(false);
   const router = useRouter();
 
-  collapsed = collapsed || router.pathname === "/explore";
-
-  React.useEffect(() => {
-    const onScroll = () => {
-      setCollapsed(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50);
-    };
-    window.addEventListener("scroll", onScroll);
-    return () => window.removeEventListener("scroll", onScroll);
-  }, []);
+  const isHome = router.pathname === "/";
 
   return (
     <>
-      <header className="bg-white border-b static md:fixed top-0 right-0 left-0 pl-3 sm:pr-4 md:pr-10 shadow-sm z-[10000]">
+      <header className={`bg-white border-b static sm:pr-4 pr-6 pl-3  shadow-sm z-[10000]`}>
         <div>
-          <div className="sm:flex justify-between py-2 items-center">
-            <div className="flex pr-8 justify-between">
-              <Link href="/">
-                <a className="flex gap-2 items-center">
-                  <Logo className={`w-[50px] ${!collapsed ? "md:w-[85px]" : ""} transition-all duration-300 h-auto`} />
-                  <div className="flex flex-col justify-center">
-                    <h1
-                      className={`text-lg ${!collapsed ? "md:text-3xl" : ""} text-gray-900 transition-all duration-300`}
-                    >
-                      Birding Hotspots
-                    </h1>
-                    <em className="text-[0.8em] leading-4 text-[#92ad39] font-medium">Where to Go Birding</em>
-                  </div>
-                </a>
-              </Link>
-              <button type="button" onClick={() => setShowSearch(true)} className="sm:hidden">
-                <MagnifyingGlassIcon className="h-5 w-5" />
-              </button>
-            </div>
-            <nav className="mt-2 md:mt-0">
+          <div className="flex py-2 items-center">
+            <Link href="/">
+              <a className="flex gap-2 items-center">
+                <Logo className={`w-[50px] ${isHome ? "md:w-[85px]" : ""} transition-all duration-300 h-auto`} />
+                <div className="flex flex-col justify-center">
+                  <h1 className={`text-lg ${isHome ? "md:text-3xl" : ""} text-gray-900 transition-all duration-300`}>
+                    Birding Hotspots
+                  </h1>
+                  <em className="text-[0.8em] leading-4 text-[#92ad39] font-medium">Where to Go Birding</em>
+                </div>
+              </a>
+            </Link>
+            <button type="button" onClick={() => setShowSearch(true)} className="ml-auto mr-6 sm:mr-4">
+              <MagnifyingGlassIcon className="h-5 w-5" />
+            </button>
+            <nav>
               <ul className="flex gap-7">
-                <li className="items-center justify-center hidden sm:flex">
-                  <button type="button" onClick={() => setShowSearch(true)}>
-                    <MagnifyingGlassIcon className="h-5 w-5" />
-                  </button>
+                <li>
+                  <Link href="/explore">
+                    <a className="text-xs font-bold text-gray-700 hover:text-gray-600 cursor-pointer uppercase">
+                      <span className="hidden sm:inline">Explore Hotspots</span>
+                      <span className="sm:hidden">Explore</span>
+                    </a>
+                  </Link>
                 </li>
-                {links.map((item) => (
-                  <NavItem key={item.label} {...item} />
-                ))}
               </ul>
             </nav>
           </div>
