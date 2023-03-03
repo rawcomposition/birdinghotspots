711770364c8b4993726c7770247a95cf30c549cf
Adam Jackson <akjackson1@gmail.com>
2023-03-03 17:16:58 -0500

    Proof of concept

--- lib/helpers.tsx
+++ lib/helpers.tsx
@@ -11,19 +11,62 @@ export function slugify(title?: string) {
   return slug.endsWith("-") ? slug.slice(0, -1) : slug;
 }
 
+const gpsModalConfig = (editor: any) => ({
+  title: "Link to Google Maps",
+  body: {
+    type: "panel",
+    items: [
+      {
+        type: "input",
+        name: "latlng",
+        label: "Enter Lat/Lng (e.g. 40.712,-74.006)",
+      },
+    ],
+  },
+  buttons: [
+    {
+      type: "cancel",
+      name: "closeButton",
+      text: "Cancel",
+    },
+    {
+      type: "submit",
+      name: "submitButton",
+      text: "Insert",
+      buttonType: "primary",
+    },
+  ],
+  onSubmit: (api: any) => {
+    const data = api.getData();
+    const latlng = data.latlng;
+
+    editor.execCommand(
+      "mceInsertContent",
+      false,
+      `<a href='https://www.google.com/maps/search/?api=1&query=${latlng}' class='map-link mceNonEditable' target='_blank'>View Map</a>`
+    );
+    api.close();
+  },
+});
+
 export const tinyConfig = {
   menubar: false,
-  plugins: "link autoresize lists",
-  toolbar: "bold italic underline bullist link",
-  content_style:
-    "body { font-family:Helvetica,Arial,sans-serif; font-size:14px } cite { font-size: 0.75em; font-style: normal; color: #666; }", //TODO: Remove cite styles a later point
+  plugins: "link autoresize lists noneditable",
+  toolbar: "bold italic underline bullist link | insertgps",
+  content_style: `body { font-family:Helvetica,Arial,sans-serif; font-size:14px } .map-link { display: inline-block; background: #f5f5f5; padding: 1px 4px; border-radius: 3px; color: #333; text-decoration: none; } .map-link::before { content: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" width="15" style="fill: rgb(194,65,13)" class="icon"><path d="M408 119.1C408 128.6 406.2 138.3 402.1 148.6C397.3 166.1 387.4 187.5 375.6 207.8L375.5 207.1C372.4 213.3 369.2 218.7 365.9 224C361.5 231 356.9 237.1 352.4 244.8L352 245.4C333.9 272.3 315.3 296.4 302.8 311.1C295.1 321.6 280.9 321.6 273.2 311.1C260.7 296.4 242.1 272.3 224 245.4C205.6 218.1 187.7 187.9 177.3 160.9C176.5 158.8 175.7 156.7 174.1 154.6C170.6 142 168 130.3 168 120C168 115.3 168.3 110.7 168.8 106.2C175.6 46.44 226.4 0 288 0C354.3 0 408 53.73 408 120V119.1zM288 151.1C310.1 151.1 328 134.1 328 111.1C328 89.91 310.1 71.1 288 71.1C265.9 71.1 248 89.91 248 111.1C248 134.1 265.9 151.1 288 151.1zM352 300.6C365.5 282.4 380.8 260.7 394.7 238.2C396.5 235.3 398.2 232.4 400 229.5V453.7L528 407.2V154.3L419.3 193.8C421.5 189.1 423.6 184.5 425.6 179.8C431.5 165.8 436.6 150.7 438.8 135.6L543.8 97.44C551.2 94.77 559.4 95.85 565.8 100.3C572.2 104.8 576 112.2 576 119.1V424C576 434.1 569.7 443.1 560.2 446.6L384.2 510.6C378.9 512.5 373.1 512.5 367.8 510.6L200 449.5L32.2 510.6C24.84 513.2 16.64 512.2 10.23 507.7C3.819 503.2 0 495.8 0 488V183.1C0 173.9 6.314 164.9 15.8 161.4L136 117.7C136 118.5 136 119.2 136 119.1C136 135.1 139.7 150.7 144.9 165.6L48 200.8V453.7L176 407.2V229.5C177.8 232.4 179.5 235.3 181.3 238.2C195.2 260.7 210.5 282.4 224 300.6V407.2L352 453.7V300.6z" /></svg>'); margin-right: 4px; }`,
   branding: false,
   elementpath: false,
-  valid_elements: "p,a[href|rel|target],strong/b,em/i,u,strike,br,ul,ol,li,cite", //TODO: Remove cite at a later point
+  valid_elements: "p,a[href|rel|target|class],strong/b,em/i,u,strike,br,ul,ol,li,cite", //TODO: Remove cite at a later point
   autoresize_bottom_margin: 0,
   convert_urls: false,
   browser_spellcheck: true,
   contextmenu: false,
+  setup: (editor: any) => {
+    editor.ui.registry.addButton("insertgps", {
+      text: "Google Maps Link",
+      onAction: () => editor.windowManager.open(gpsModalConfig(editor)),
+    });
+  },
 };
 
 export function capitalize(str: string) {
