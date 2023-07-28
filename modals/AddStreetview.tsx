550cb4c899fca1235a74a6da5e71eb3137348a73
Adam Jackson <akjackson1@gmail.com>
2023-07-28 16:37:28 -0400

    Autofocus streetview url

--- modals/AddStreetview.tsx
+++ modals/AddStreetview.tsx
@@ -15,6 +15,7 @@ export default function AddStreetView({ locationId, onSuccess }: Props) {
   const [fov, setFov] = React.useState(70);
   const { send, loading } = useToast();
   const { close } = useModal();
+  const inputRef = React.useRef<HTMLInputElement>(null);
 
   const pieces = url.split(",");
 
@@ -56,12 +57,22 @@ export default function AddStreetView({ locationId, onSuccess }: Props) {
     setFov(90);
   };
 
+  React.useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+
   return (
     <>
       <div className="flex-1">
         <label className="text-gray-500 font-bold">
           Google Street View URL <br />
-          <input type="text" className="form-input" value={url} onChange={(e) => setUrl(e.target.value)} />
+          <input
+            type="text"
+            className="form-input"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            ref={inputRef}
+          />
         </label>
       </div>
       {!url && (
