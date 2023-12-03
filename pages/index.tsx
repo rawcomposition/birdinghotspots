708b6cc5ac7884f97be3e241b4724b1249789987
Adam Jackson <akjackson1@gmail.com>
2023-12-03 10:20:28 -0500

    Activate Faroe Islands

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,8 +47,11 @@ export default function Home({ featured, northAmericaRegions }: Props) {
             ))}
             <h3 className="text-lg mb-4 font-bold">Worldwide</h3>
             <div className="columns-2 lg:columns-3 mb-12">
-              <Link href={`/region/NA`} className="font-bold px-2 py-1 text-base mb-1 block">
+              <Link href="/region/NA" className="font-bold px-2 py-1 text-base mb-1 block">
                 Namibia
+              </Link>
+              <Link href="/region/FO" className="font-bold px-2 py-1 text-base mb-1 block">
+                Faroe Islands
               </Link>
             </div>
           </section>
