e62446f2ef98a1177f0cbc9e4458e70083a16318
Adam Jackson <akjackson1@gmail.com>
2024-01-31 16:31:46 -0800

    Default to next available region

--- lib/localData.ts
+++ lib/localData.ts
@@ -21,6 +21,7 @@ export function getRegion(code: string): Region | null {
   if (!code) return null;
   const regions = Regions as Region[];
   const pieces = code.split("-");
+  let region = null;
 
   if (pieces.length === 3) {
     // County
@@ -29,62 +30,66 @@ export function getRegion(code: string): Region | null {
     const countyCode = code;
 
     const country = regions.find((it) => it.code === countryCode);
-    if (!country) return null;
-    const state = country.subregions?.find((it) => it.code === stateCode);
-    if (!state) return null;
-    const county = state.subregions?.find((it) => it.code === countyCode);
-    if (!county) return null;
+    const state = country?.subregions?.find((it) => it.code === stateCode);
+    const county = state?.subregions?.find((it) => it.code === countyCode);
 
-    return formatRegion({
-      ...county,
-      features: state.features || [],
-      ...(state.portal ? { portal: state.portal } : {}),
-      longName: county.longName || `${county.name} County`,
-      parents: [
-        {
-          code: state.code,
-          name: state.name,
-        },
-        {
-          code: country.code,
-          name: country.name,
-        },
-      ],
-    });
+    if (county && state && country) {
+      region = formatRegion({
+        ...county,
+        features: state.features || [],
+        ...(state.portal ? { portal: state.portal } : {}),
+        longName: county.longName || `${county.name} County`,
+        parents: [
+          {
+            code: state.code,
+            name: state.name,
+          },
+          {
+            code: country.code,
+            name: country.name,
+          },
+        ],
+      });
+    }
   } else if (pieces.length === 2) {
     // State
     const countryCode = pieces[0];
     const stateCode = code;
 
     const country = Regions.find((it) => it.code === countryCode);
-    if (!country) return null;
-    const state = country.subregions?.find((it) => it.code === stateCode);
-    if (!state) return null;
+    const state = country?.subregions?.find((it) => it.code === stateCode);
 
-    return formatRegion({
-      ...state,
-      longName: state.name,
-      parents: [
-        {
-          code: country.code,
-          name: country.name,
-        },
-      ],
-    });
+    if (state && country) {
+      region = formatRegion({
+        ...state,
+        longName: state.name,
+        parents: [
+          {
+            code: country.code,
+            name: country.name,
+          },
+        ],
+      });
+    }
   } else if (pieces.length === 1) {
     // Country
     const countryCode = code;
 
     const country = Regions.find((it) => it.code === countryCode) as Region;
-    if (!country) return null;
 
-    return formatRegion({
-      ...country,
-      longName: country.name,
-      subregions: country.subregions?.map(({ subregions, ...rest }) => rest),
-    });
+    if (country) {
+      region = formatRegion({
+        ...country,
+        longName: country.name,
+        subregions: country.subregions?.map(({ subregions, ...rest }) => rest),
+      });
+    }
   }
-  return null;
+
+  if (!region && pieces.length > 1) {
+    return getRegion(pieces.slice(0, pieces.length - 1).join("-"));
+  }
+  return region || null;
 }
 
 type DriveMap = {
