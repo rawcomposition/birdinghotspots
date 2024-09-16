8d51a248ff195b9bc768426ee51aef1d86d56518
Adam Jackson <akjackson1@gmail.com>
2024-09-15 20:15:59 -0700

    Show square preview

--- components/InputImageCrop.tsx
+++ components/InputImageCrop.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { useController } from "react-hook-form";
 import Cropper from "react-easy-crop";
 import React from "react";
@@ -70,28 +71,30 @@ export default function InputImageCrop({ className, name, url }: Props) {
   );
 }
 
-const Preview = ({
-  x,
-  y,
-  width,
-  url,
-  square,
-}: {
+type PreviewProps = {
   x: number;
   y: number;
   width: number;
   url: string;
   square?: boolean;
-}) => {
+};
+
+const Preview = ({ x, y, width, url, square }: PreviewProps) => {
   const scale = 100 / width;
-  const transform = `translate3d(${-x * scale}%, ${-y * scale}%, 0) scale3d(${scale}, ${scale}, 1)`;
+  const transform = `translate3d(${-x * scale}%, ${-y * scale}%, 0) scale(${scale})`;
+  const containerStyle = square ? { aspectRatio: "1 / 1" } : { aspectRatio: "3 / 2" };
 
   return (
-    <div className="h-[160px] aspect-[3/2] relative overflow-hidden">
+    <div className="h-[160px] relative overflow-hidden" style={containerStyle}>
       <img
         src={url}
         alt=""
-        style={{ transform, width: "calc(100% + 0.5px)", height: "auto" }}
+        style={{
+          transform,
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+        }}
         className="absolute top-0 left-0 origin-top-left"
       />
     </div>
