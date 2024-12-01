b8c550178a28edb87f1223b86d73e0bec2148c1d
Adam Jackson <akjackson1@gmail.com>
2024-12-01 08:23:55 -0800

    Remove unused files

--- pages/api/species/sync-source-info.ts
+++ pages/api/species/sync-source-info.ts
@@ -4,6 +4,7 @@ import Species from "models/Species";
 import { getSourceInfo } from "lib/species";
 
 const DRY_RUN = true;
+const LIMIT = 10;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
   if (process.env.NODE_ENV !== "development") {
@@ -12,18 +13,35 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 
   await connect();
 
-  const results = await Species.find({}).sort({ order: 1 }).lean();
+  const results = await Species.find({ source: "inat" }).sort({ order: 1 }).limit(LIMIT).lean();
 
   const bulkWrites: any[] = [];
 
   for (const species of results) {
     const info = await getSourceInfo(species.source, species.sourceId, species.iNatObsId);
+    if (!info) {
+      console.log("NO INFO", species._id);
+      continue;
+    }
+    const { author, license, iNatUserId, speciesName } = info;
+    if (speciesName !== species.sciName) {
+      console.log("NAME MISMATCH", species._id);
+      continue;
+    }
+    if (author !== species.author) {
+      console.log("Author update", species.author, "->", author);
+    }
+    if (license !== species.license) {
+      console.log("License update", species.license, "->", license);
+    }
+    bulkWrites.push({ updateOne: { filter: { _id: species._id }, update: { $set: { author, license, iNatUserId } } } });
   }
 
   if (DRY_RUN) {
     console.log("Total writes", bulkWrites.length);
   } else {
     await Species.bulkWrite(bulkWrites);
+    console.log("Total writes", bulkWrites.length);
   }
 
   res.status(200).json({ success: true });
