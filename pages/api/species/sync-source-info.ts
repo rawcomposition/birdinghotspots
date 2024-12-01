e15dc54c9f4809ec76c2238a1b1b15ab09263d6b
Adam Jackson <akjackson1@gmail.com>
2024-12-01 10:23:54 -0800

    Update sync-source-info.ts

--- pages/api/species/sync-source-info.ts
+++ pages/api/species/sync-source-info.ts
@@ -3,8 +3,10 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import Species from "models/Species";
 import { getSourceInfo } from "lib/species";
 
-const DRY_RUN = true;
-const LIMIT = 10;
+const DRY_RUN = false;
+const LIMIT = 10000;
+const REQUESTS_PER_MINUTE = 50;
+const DELAY = 60000 / REQUESTS_PER_MINUTE;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
   if (process.env.NODE_ENV !== "development") {
@@ -13,35 +15,48 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 
   await connect();
 
-  const results = await Species.find({ source: "inat" }).sort({ order: 1 }).limit(LIMIT).lean();
-
-  const bulkWrites: any[] = [];
+  const results = await Species.find({ source: "inat", iNatUserId: { $exists: false } })
+    .sort({ order: 1 })
+    .limit(LIMIT)
+    .lean();
 
   for (const species of results) {
-    const info = await getSourceInfo(species.source, species.sourceId, species.iNatObsId);
-    if (!info) {
-      console.log("NO INFO", species._id);
+    const startTime = Date.now();
+    try {
+      const info = await getSourceInfo(species.source, species.sourceId, species.iNatObsId);
+
+      if (!info) {
+        console.log("NO INFO", species._id);
+        continue;
+      }
+      const { author, license, iNatUserId, speciesName } = info;
+      if (speciesName !== species.sciName) {
+        console.log("NAME MISMATCH", species._id, species.sciName, "->", speciesName);
+        continue;
+      }
+      if (author !== species.author) {
+        console.log("Author update", species._id, species.author, "->", author);
+      }
+      if (license !== species.license) {
+        console.log("License update", species._id, species.license, "->", license);
+      }
+      await Species.updateOne({ _id: species._id }, { $set: { author, license, iNatUserId } });
+
+      const elapsedTime = Date.now() - startTime;
+      const remainingDelay = DELAY - elapsedTime;
+      if (remainingDelay > 0) {
+        await new Promise((resolve) => setTimeout(resolve, remainingDelay)); // Throttle requests
+      }
+    } catch (e) {
+      console.log("ERROR", species._id, e);
       continue;
     }
-    const { author, license, iNatUserId, speciesName } = info;
-    if (speciesName !== species.sciName) {
-      console.log("NAME MISMATCH", species._id);
-      continue;
-    }
-    if (author !== species.author) {
-      console.log("Author update", species.author, "->", author);
-    }
-    if (license !== species.license) {
-      console.log("License update", species.license, "->", license);
-    }
-    bulkWrites.push({ updateOne: { filter: { _id: species._id }, update: { $set: { author, license, iNatUserId } } } });
   }
 
   if (DRY_RUN) {
-    console.log("Total writes", bulkWrites.length);
+    console.log("DONE");
   } else {
-    await Species.bulkWrite(bulkWrites);
-    console.log("Total writes", bulkWrites.length);
+    console.log("DONE");
   }
 
   res.status(200).json({ success: true });
