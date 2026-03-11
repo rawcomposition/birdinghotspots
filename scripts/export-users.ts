3fe3500e39348ce674fc7f927d15ecd84e814b6c
Adam Jackson <akjackson1@gmail.com>
2026-03-10 18:47:59 -0700

    Update export-users.ts

--- scripts/export-users.ts
+++ scripts/export-users.ts
@@ -9,6 +9,7 @@ dotenv.config();
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
+      // @ts-ignore
       project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       private_key: process.env.FIREBASE_PRIVATE_KEY,
       client_email: process.env.FIREBASE_CLIENT_EMAIL,
